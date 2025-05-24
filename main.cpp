4491e93afb0823bd4c6378ade3ce5d15cbf50be2
PiPower <darthlord.pl@gmail.com>
2025-05-25 00:54:44 +0200

    fixed camera flipping

--- main.cpp
+++ main.cpp
@@ -1,32 +1,33 @@
 #include "window.hpp"
 #include "VulkanRenderer.hpp"
+#include "Scene.hpp"
 
-void updatePosition(Window* window, glm::vec3* eye, glm::vec3* centerVec, glm::vec3* up, glm::vec3* centerDir);
+void updatePosition(Window* window, glm::vec3* eye, glm::vec3* centerVec, glm::vec3* up, glm::vec3* centerDir, glm::vec3* upLook);
 int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {
-    Window* wnd = new Window(1000, 1000, L"yolo", L"test");
+    Window* wnd = new Window(1600, 900, L"yolo", L"test");
     VulkanRenderer* renderer = new VulkanRenderer(hInstance, wnd->GetWindowHWND());
-    glm::vec3 eye{ 0.0f, 0.0f, 0.0f }, centerDir{ 0.0f, 0.0f, 1.0f }, up{ 0.0f, 1.0f, 0.0f };
+    glm::vec3 eye{ 0.0f, 0.0f, -20.0f }, centerDir{ 0.0f, 0.0f, 1.0f }, up{ 0.0f, 1.0f, 0.0f }, upLook{ 0.0f, 1.0f, 0.0f };
     glm::vec3 center = eye + centerDir;
     renderer->updateRotation();
     while (wnd->ProcessMessages() == 0)
     {
-        renderer->updateCameraLH(eye, center, up);
+        renderer->updateCameraLH(eye, center, upLook);
         renderer->Render();
-        updatePosition(wnd, &eye, &center, &up, &centerDir);
+        updatePosition(wnd, &eye, &center, &up, &centerDir, &upLook);
     }
     
 }
 
-void updatePosition(Window* window, glm::vec3* eye, glm::vec3* center, glm::vec3* up, glm::vec3* centerDir)
+void updatePosition(Window* window, glm::vec3* eye, glm::vec3* center, glm::vec3* up, glm::vec3* centerDir, glm::vec3* upLook)
 {
     static float angleX = 0.0f, angleY =0.0f;
     if (window->IsKeyPressed('W')) { *eye += 0.001f * (*centerDir); }
     if (window->IsKeyPressed('S')) { *eye -= 0.001f * (*centerDir); }
     if (window->IsKeyPressed(VK_SPACE)) { *eye += 0.001f * (*up); }
     if (window->IsKeyPressed(VK_CONTROL)) { *eye -= 0.001f * (*up); }
-    if (window->IsKeyPressed('D')){ *eye += 0.001f * glm::cross(*up, *centerDir);}
-    if (window->IsKeyPressed('A')){ *eye -= 0.001f * glm::cross(*up, *centerDir); }
+    if (window->IsKeyPressed('D')){ *eye += 0.001f * glm::cross(*upLook, *centerDir);}
+    if (window->IsKeyPressed('A')){ *eye -= 0.001f * glm::cross(*upLook, *centerDir); }
     if (window->IsLeftPressed())
     {
         if (window->GetMouseDeltaX() < 0)
@@ -51,8 +52,11 @@ void updatePosition(Window* window, glm::vec3* eye, glm::vec3* center, glm::vec3
                         glm::rotate(glm::mat4(1.0f), angleY, glm::vec3(0.0f, 1.0f, 0.0f));
 
         glm::vec4 pos(0.0f, 0.0f, 1.0f, 0.0f);
+        glm::vec4 upCamera(0.0f, 1.0f, 0.0f, 0.0f);
         pos = pos * rot;
+        upCamera = upCamera * rot;
         *centerDir = glm::vec3(pos);
+        *upLook = glm::vec3(upCamera);
     }
     *center = *eye + *centerDir;
 }
